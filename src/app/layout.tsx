1ee8079b0db01b1d309d4e713325102be9b19227
theonlyamjad <amjadmessi2004@gmail.com>
2026-03-16 20:54:22 +0000

    feat : Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "../components/Header";
 import ViewCanvas from "../components/ViewCanvas";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 const alpino = localFont({
   src: "../fonts/Alpino-Variable.woff2",
@@ -28,6 +29,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </main>
         <Footer/>
       </body>
+      <Analytics/>
     </html>
   );
 }
